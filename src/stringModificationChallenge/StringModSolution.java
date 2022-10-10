d4db3cf12bf841fd2aed9ab19bc98ee04365cd51
Alexander Klubnikov <clubman066@gmail.com>
2022-10-10 10:11:15 +0300

    Correcting mistakes of string modification challenge

--- src/stringModificationChallenge/StringModSolution.java
+++ src/stringModificationChallenge/StringModSolution.java
@@ -4,49 +4,68 @@ public class StringModSolution {
     public static void main(String[] args) {
         String str1 = "abc";
         String str2 = "abc";
-        System.out.println(isModifiable(str1, str2));
+        System.out.println(isOneModification(str1, str2));
     }
 
-    private static boolean isModifiable(String string1, String string2) {
+    private static boolean isOneModification(String string1, String string2) {
 
         int lengthSubtraction = Math.abs(string1.length() - string2.length());
-        boolean result = false;
+        boolean result=false;
 
-        if (lengthSubtraction >= 2) {
-            return result;
+        if (lengthSubtraction > 1) {
+            return false;
+
         } else if (lengthSubtraction == 0) {
-            char[] charArr1 = string1.toCharArray();
-            char[] charArr2 = string2.toCharArray();
-            int modifyCounter = 0;
+            result = countDiffSymbols(string1, string2) <= 1;
 
-            for (int i = 0; i < string1.length(); i++) {
-                if (charArr1[i] != charArr2[i]) {
-                    modifyCounter++;
-                }
-            }
-            result = modifyCounter <= 1;
-        } else if (lengthSubtraction == 1) {
-            int similarSymbolCounter = 0;
-            char[] smallArray = getSmallestArray(string1, string2);
-            char[] bigArray = getBiggestArray(string1, string2);
-
-            for (char char1 : smallArray) {
-                for (char char2 : bigArray) {
-                    if (char1 == char2) {
-                        similarSymbolCounter++;
-                    }
-                }
-            }
-            result = similarSymbolCounter == smallArray.length;
+        } else {
+            String smallString = getSmallestString(string1, string2);
+            String bigString = getBiggestString(string1, string2);
+            if (countSimilarSymbols(smallString, bigString) == smallString.length()
+                    || countSimilarSymbols(smallString, bigString) == smallString.length()+1)
+                result = true;
         }
         return result;
     }
 
-    private static char[] getSmallestArray(String string1, String string2) {
-        return string1.length() - string2.length() < 0 ? string1.toCharArray() : string2.toCharArray();
+    private static String getSmallestString(String string1, String string2) {
+        return string1.length() - string2.length() < 0 ? string1 : string2;
     }
 
-    private static char[] getBiggestArray(String string1, String string2) {
-        return string1.length() - string2.length() > 0 ? string1.toCharArray() : string2.toCharArray();
+    private static String getBiggestString(String string1, String string2) {
+        return string1.length() - string2.length() > 0 ? string1 : string2;
+    }
+
+    private static int countDiffSymbols(String string1, String string2) {
+        char[] charArr1 = string1.toCharArray();
+        char[] charArr2 = string2.toCharArray();
+        int modifyCounter = 0;
+
+        for (int i = 0; i < string1.length(); i++) {
+            if (charArr1[i] != charArr2[i]) {
+                modifyCounter++;
+            }
+            if (modifyCounter > 1)
+                break;
+        }
+        return modifyCounter;
+    }
+
+    private static int countSimilarSymbols(String smallString, String bigString) {
+        int similarSymbolCounter = 0;
+        char[] smallArray = smallString.toCharArray();
+        char[] bigArray = bigString.toCharArray();
+
+        for (int i = 0; i < smallArray.length; i++) {
+            for (int j = i; j < bigArray.length; j++) {
+                if (smallArray[i] == bigArray[j]) {
+                    similarSymbolCounter++;
+                }
+                if (similarSymbolCounter > smallArray.length) {
+                    break;
+                }
+            }
+        }
+        return similarSymbolCounter;
     }
 }
